4d03dc6d2c3e288c2b26ae655215979a58747895
Anrighe <assoonpc@gmail.com>
2023-11-12 13:26:51 +0100

    working

--- hash_table.h
+++ hash_table.h
@@ -3,6 +3,9 @@
 
 #define HASH_TABLE_STD_SIZE 1024
 
+/// @brief Simple hash function that sums the ascii values of the characters in the string and returns the sum as the hash value
+/// @param key String used to calculate the hash value
+/// @return integer corresponding to the hash value
 int hash_function(const char * key)
 {   
     int hash = 0;
@@ -15,58 +18,67 @@ int hash_function(const char * key)
     return hash;
 }
 
-//TODO: THIS MIGHT BE ELIMINATED?
-struct hash_table_entry
-{
-    void * element;
-};
-
+/// @brief Structure that represents a hash table
 struct hash_table
 {
-    struct hash_table_entry * table;
+    void ** table; // void ** is used to store any type of pointer
     int size;
 };
 
-void hash_table_insert(struct hash_table table, const char * key, void * element)
+/// @brief Inserts an element into the hash table. If the hash value is greater than the size of the table, 
+///         the table is resized so that it can fit the newer element. This function does not check if the 
+///         element is already in the table
+/// @param table Hash table to insert the element into
+/// @param key Key used to calculate the hash value
+/// @param element Element to insert into the hash table
+void hash_table_insert(struct hash_table * table, const char * key, void * element)
 {
-    int hash = hash_function(key);
-
-    // need to check if the current table is less than the hash key number
-    // if it is, then we need to resize the table
-    // if not, then we can just insert the element into the table
-    // but first we need to check if the 
+    int hash = hash_function(key); 
     
-    
-    if (table.size <= hash) // the table is too small -> needs resizing
+    if (table->size <= hash) // the table is too small -> needs resizing
     {
-        // copy the table
-        struct hash_table_entry * new_table = malloc(sizeof(struct hash_table_entry) * (hash + 1));
+        // copy the old table values to the newer one
+        void ** new_table = malloc(sizeof(void*) * (hash + 1));
 
-        for (size_t i = 0; i < table.size; ++i)
-            new_table[i] = table.table[i];
+        for (size_t i = 0; i < hash + 1; ++i)
+            new_table[i] = NULL;
 
-        free(table.table);
+        for (size_t i = 0; i < table->size; ++i)
+            new_table[i] = table->table[i];
 
-        table.size = hash + 1;
+        free(table->table);
+
+        table->table = new_table;
+        table->size = hash + 1;
     }
 
-    table.table[hash].element = element;
+    table->table[hash] = element;
 };
 
-void * hash_table_get(struct hash_table table, const char * key)
+/// @brief Gets an element from the hash table and returns it. If the hash value is greater than the table size,
+///         the function returns NULL
+/// @param table Hash table to get the element from
+/// @param key Key used to calculate the hash value
+/// @return A pointer to the element in the hash table
+void * hash_table_get(struct hash_table * table, const char * key)
 {
     int hash = hash_function(key);
-
-    if (table.size <= hash)
+    if (table->size <= hash)
         return NULL;
 
-    return table.table[hash].element;
+    return table->table[hash];
 }
 
+/// @brief Generates a hash table with the standard size `HASH_TABLE_STD_SIZE`. Initializes each element to `NULL and returns it
+/// @return A hash table with the standard size `HASH_TABLE_STD_SIZE`
 struct hash_table hash_table_init()
 {
     struct hash_table table;
-    table.table = malloc(sizeof(struct hash_table_entry) * HASH_TABLE_STD_SIZE);
+    table.table = malloc(sizeof(void *) * HASH_TABLE_STD_SIZE);
+
+    for (size_t i = 0; i < HASH_TABLE_STD_SIZE; ++i)
+        table.table[i] = NULL;
+
     table.size = HASH_TABLE_STD_SIZE;
 
     return table;
--- test/test.c
+++ test/test.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include "../hash_table.h"
 
-
 int main()
 {
 
-    char * test_string = "Helloo";
+    char * test_string = "Helloo 123a11a";
 
-    int element = 4;
+    printf("Hash value corresponding to the key '%s' is: %d\n", test_string, hash_function(test_string));
 
-
-    //printf("%d\n", hash_function(test_string));
+    int element = 4123;
 
     struct hash_table table = hash_table_init();
-    hash_table_insert(table, test_string, &element);
+    printf("Before hash_table_insert\n");
 
-    printf("%d\n", hash_table_get(table, test_string));
+    hash_table_insert(&table, test_string, &element);
+
+    printf("Before hash_table_get\n");
+    printf("The requested element corresponding to the key '%s' is: %d\n", test_string, *(int*)hash_table_get(&table, test_string));
 
     return 0;
 }
